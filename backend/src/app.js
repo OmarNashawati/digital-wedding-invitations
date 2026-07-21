72020fa0e9069e5f6b950c36aa201d6a2a089977
Omar Nashawati <oceanprime.on@gmail.com>
2026-07-21 22:24:34 +0300

    add routes to app.js test routes and fix bugs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,16 @@
 import express from 'express';
+import authRoute from './routers/auth.js';
+import invitationsRoute from './routers/invitations.js';
+import rsvpRoute from './routers/rsvp.js';
+import templatesRoute from './routers/templates.js';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/v1/auth', authRoute);
+app.use('/api/v1/invitations', invitationsRoute);
+app.use('/api/v1/rsvp', rsvpRoute);
+app.use('/api/v1/templates', templatesRoute);
 
 export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-import app from './app.JS';
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
